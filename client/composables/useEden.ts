bb951293ecc5109b30ae9444380b94cb4fcab615
Wesam Jabali <wesamjabali@icloud.com>
2024-02-11 00:29:14 +0100

    new chat api url

--- client/composables/useEden.ts
+++ client/composables/useEden.ts
@@ -1,6 +1,6 @@
 import { edenTreaty } from "@elysiajs/eden";
 import type { App } from "../../server/src/index";
-const eden = edenTreaty<App>('http://66.42.126.7:3000');
+const eden = edenTreaty<App>('http://chatapi.wesamjabali.com:3000');
 export function useEden() {
     return { eden }
 }
