da4e23c7da758fec32c043ccd77227db4f9be084
Wesam Jabali <wesamjabali@icloud.com>
2024-02-11 00:01:45 +0100

    add root

--- server/src/index.ts
+++ server/src/index.ts
@@ -90,7 +90,10 @@ const app = new Elysia()
       }
     },
   })
-  .listen({ hostname: "0.0.0.0", port: 3000 });
+  .get('/', () => {
+    return 'Hello world.'
+  })
+  .listen({ hostname: "0.0.0.0", port: 3000 })
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
