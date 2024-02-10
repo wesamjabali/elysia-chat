0fd97ecbc74e7a01ade962cf74d42436bf778058
Wesam Jabali <wesamjabali@icloud.com>
2024-02-10 23:44:41 +0100

    set host 0.0.0.0

--- server/src/index.ts
+++ server/src/index.ts
@@ -90,7 +90,7 @@ const app = new Elysia()
       }
     },
   })
-  .listen(3000);
+  .listen({ hostname: "0.0.0.0", port: 3000 });
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
