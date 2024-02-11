cce5a1d51026fff28f604287295cae1db0b4b445
Wesam Jabali <wesamjabali@icloud.com>
2024-02-11 14:15:46 +0100

    change port in client

--- client/composables/useEden.ts
+++ client/composables/useEden.ts
@@ -1,6 +1,6 @@
 import { edenTreaty } from "@elysiajs/eden";
 import type { App } from "../../server/src/index";
-const eden = edenTreaty<App>('https://chatapi.wesamjabali.com:3000');
+const eden = edenTreaty<App>('https://chatapi.wesamjabali.com:8443');
 export function useEden() {
     return { eden }
 }
